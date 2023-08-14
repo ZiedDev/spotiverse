b6d79c0752f88b70ab775e8683753b1f7d871af6
ZiedDev <73042811+ZiedDev@users.noreply.github.com>
2023-08-15 01:48:41 +0300

    v0.1.4.1
    Added 3 songs to the app.

--- artists.js
+++ artists.js
@@ -92,4 +92,71 @@ let artistsObject = {
             }
         }
     },
+
+    // TO CHANGE MUSIC HERE 31415
+
+    'The Weeknd': {
+        'Starboy': {
+            name: 'Starboy',
+            year: 2016,
+            artists: ["The Weeknd"],
+            length: 18,
+            songs: {
+                1: {
+                    name: "Starboy",
+                    artists: "The Weeknd, Daft Punk",
+                    duration: 230
+                }
+            }
+        }
+    },
+
+    'Travis Scott, Bad Bunny, The Weeknd': {
+        'K-POP': {
+            name: 'K-POP',
+            year: 2023,
+            artists: ["Travis Scott, Bad Bunny, The Weeknd"],
+            length: 1,
+            songs: {
+                1: {
+                    name: "K-POP",
+                    artists: "Travis Scott, Bad Bunny, The Weeknd",
+                    duration: 185
+                }
+            }
+        }
+    },
+
+    'Post Malone': {
+        'Twelve Carat Toothache': {
+            name: 'Twelve Carat Toothache',
+            year: 2022,
+            artists: ["Post Malone"],
+            length: 14,
+            songs: {
+                1: {
+                    name: "One Right Now",
+                    artists: "Post Malone, The Weeknd",
+                    duration: 193
+                }
+            }
+        }
+    },
+
+    'Post Malone': {
+        'beerbongs & bentleys': {
+            name: 'beerbongs & bentleys',
+            year: 2018,
+            artists: ["Post Malone"],
+            length: 18,
+            songs: {
+                1: {
+                    name: "Rockstar ft. 21 Savage",
+                    artists: "Post Malone, 21 Savage",
+                    duration: 218
+                }
+            }
+        }
+    },
+
 };
--- script.js
+++ script.js
@@ -15,60 +15,60 @@ const queueContent = document.getElementById("queue-content");
 
 // Nav buttons
 function resetButtonsStyle() {
-    accountButton.classList.remove("clicked-button");
-    homeButton.classList.remove("clicked-button");
-    searchButton.classList.remove("clicked-button");
-    libraryButton.classList.remove("clicked-button");
-    queueButton.classList.remove("clicked-button");
+  accountButton.classList.remove("clicked-button");
+  homeButton.classList.remove("clicked-button");
+  searchButton.classList.remove("clicked-button");
+  libraryButton.classList.remove("clicked-button");
+  queueButton.classList.remove("clicked-button");
 
-    accountContent.classList.add("hidden");
-    homeContent.classList.add("hidden");
-    searchContent.classList.add("hidden");
-    libraryContent.classList.add("hidden");
-    albumContent.classList.add("hidden");
-    queueContent.classList.add("hidden");
+  accountContent.classList.add("hidden");
+  homeContent.classList.add("hidden");
+  searchContent.classList.add("hidden");
+  libraryContent.classList.add("hidden");
+  albumContent.classList.add("hidden");
+  queueContent.classList.add("hidden");
 }
 
 function changeToAccount() {
-    resetButtonsStyle();
-    accountButton.classList.add("clicked-button");
+  resetButtonsStyle();
+  accountButton.classList.add("clicked-button");
 
-    accountContent.classList.remove("hidden");
+  accountContent.classList.remove("hidden");
 }
 
 function changeToHome() {
-    resetButtonsStyle();
-    homeButton.classList.add("clicked-button");
+  resetButtonsStyle();
+  homeButton.classList.add("clicked-button");
 
-    homeContent.classList.remove("hidden");
+  homeContent.classList.remove("hidden");
 }
 
 function changeToAlbum(artist, albumName) {
-    resetButtonsStyle();
+  resetButtonsStyle();
 
-    albumContent.classList.remove("hidden");
-    changeAlbumContent(artist, albumName);
+  albumContent.classList.remove("hidden");
+  changeAlbumContent(artist, albumName);
 }
 
 function changeToSearch() {
-    resetButtonsStyle();
-    searchButton.classList.add("clicked-button");
+  resetButtonsStyle();
+  searchButton.classList.add("clicked-button");
 
-    searchContent.classList.remove("hidden");
+  searchContent.classList.remove("hidden");
 }
 
 function changeToLibrary() {
-    resetButtonsStyle();
-    libraryButton.classList.add("clicked-button");
+  resetButtonsStyle();
+  libraryButton.classList.add("clicked-button");
 
-    libraryContent.classList.remove("hidden");
+  libraryContent.classList.remove("hidden");
 }
 
 function changeToQueue() {
-    resetButtonsStyle();
-    queueButton.classList.add("clicked-button");
+  resetButtonsStyle();
+  queueButton.classList.add("clicked-button");
 
-    queueContent.classList.remove("hidden");
+  queueContent.classList.remove("hidden");
 }
 
 // Timeline
@@ -93,24 +93,24 @@ const full_currentTime = document.getElementById("full_current-time");
 const volumeSlider = document.getElementById("volume-slider");
 
 function playAudio() {
-    audio.play();
+  audio.play();
 }
 function pauseAudio() {
-    audio.pause();
+  audio.pause();
 }
 
 function playPauseButtonBehavior() {
-    if (isPlaying && !isLoadingAudio) {
-        playPauseButton.textContent = "play_circle";
-        full_playPauseButton.textContent = "play_circle";
-        pauseAudio();
-        isPlaying = false;
-    } else if (!isPlaying && !isLoadingAudio) {
-        playPauseButton.textContent = "pause_circle";
-        full_playPauseButton.textContent = "pause_circle";
-        playAudio();
-        isPlaying = true;
-    }
+  if (isPlaying && !isLoadingAudio) {
+    playPauseButton.textContent = "play_circle";
+    full_playPauseButton.textContent = "play_circle";
+    pauseAudio();
+    isPlaying = false;
+  } else if (!isPlaying && !isLoadingAudio) {
+    playPauseButton.textContent = "pause_circle";
+    full_playPauseButton.textContent = "pause_circle";
+    playAudio();
+    isPlaying = true;
+  }
 }
 
 let isLoadingAudio = false;
@@ -120,22 +120,22 @@ audio.addEventListener("loadstart", loadingAudio);
 audio.addEventListener("loadeddata", loadedAudio);
 
 function loadingAudio() {
-    isLoadingAudio = true;
-    if (!isLoadFirstTime) {
-        playPauseButton.classList.add("loading")
-        playPauseButton.textContent = "cached";
-        full_playPauseButton.classList.add("loading")
-        full_playPauseButton.textContent = "cached";
-    }
-    isLoadFirstTime = true
+  isLoadingAudio = true;
+  if (!isLoadFirstTime) {
+    playPauseButton.classList.add("loading")
+    playPauseButton.textContent = "cached";
+    full_playPauseButton.classList.add("loading")
+    full_playPauseButton.textContent = "cached";
+  }
+  isLoadFirstTime = true
 }
 
 function loadedAudio() {
-    isLoadingAudio = false;
-    playPauseButton.classList.remove("loading")
-    full_playPauseButton.classList.remove("loading")
-    playPauseButton.textContent = "pause_circle";
-    full_playPauseButton.textContent = "pause_circle";
+  isLoadingAudio = false;
+  playPauseButton.classList.remove("loading")
+  full_playPauseButton.classList.remove("loading")
+  playPauseButton.textContent = "pause_circle";
+  full_playPauseButton.textContent = "pause_circle";
 }
 
 let volume = volumeSlider.value / 100;
@@ -147,82 +147,82 @@ const full_volumeButton = document.getElementById("full_volume-button");
 const full_volumeSlider = document.getElementById("full_volume-slider");
 
 function muteMusic() {
-    if (isMuted) {
-        changeMusicVolume();
-        isMuted = false;
-    }
-    else {
-        audio.volume = 0;
-        volumeButton.textContent = "no_sound";
-        full_volumeButton.textContent = "no_sound";
-        isMuted = true;
-    }
+  if (isMuted) {
+    changeMusicVolume();
+    isMuted = false;
+  }
+  else {
+    audio.volume = 0;
+    volumeButton.textContent = "no_sound";
+    full_volumeButton.textContent = "no_sound";
+    isMuted = true;
+  }
 }
 
 volumeSlider.addEventListener("input", function () {
-    volume = volumeSlider.value / 100;
+  volume = volumeSlider.value / 100;
 
-    if (isMuted) {
-        muteMusic();
-    }
-    changeMusicVolume();
+  if (isMuted) {
+    muteMusic();
+  }
+  changeMusicVolume();
 });
 
 full_volumeSlider.addEventListener("input", function () {
-    volume = full_volumeSlider.value / 100;
+  volume = full_volumeSlider.value / 100;
 
-    if (isMuted) {
-        muteMusic();
-    }
-    changeMusicVolume();
+  if (isMuted) {
+    muteMusic();
+  }
+  changeMusicVolume();
 });
 
 function changeMusicVolume() {
-    audio.volume = volume;
+  audio.volume = volume;
 
-    if (audio.volume == 0) {
-        volumeButton.textContent = "volume_mute";
-        full_volumeButton.textContent = "volume_mute";
-    }
-    else if (audio.volume < 0.35) {
-        volumeButton.textContent = "volume_down";
-        full_volumeButton.textContent = "volume_down";
-    }
-    else if (audio.volume > 0.35) {
-        volumeButton.textContent = "volume_up";
-        full_volumeButton.textContent = "volume_up";
-    }
+  if (audio.volume == 0) {
+    volumeButton.textContent = "volume_mute";
+    full_volumeButton.textContent = "volume_mute";
+  }
+  else if (audio.volume < 0.35) {
+    volumeButton.textContent = "volume_down";
+    full_volumeButton.textContent = "volume_down";
+  }
+  else if (audio.volume > 0.35) {
+    volumeButton.textContent = "volume_up";
+    full_volumeButton.textContent = "volume_up";
+  }
 }
 
 audio.ontimeupdate = function () {
-    timelineSlider.max = audio.duration;
-    timelineSlider.value = audio.currentTime;
+  timelineSlider.max = audio.duration;
+  timelineSlider.value = audio.currentTime;
 
-    musicDuration.textContent =
-        Math.floor(audio.duration / 60) > 0 ? `${Math.floor(audio.duration / 60)}:${String(Math.floor(audio.duration) % 60).padStart(2, '0')}` : "0:00";
+  musicDuration.textContent =
+    Math.floor(audio.duration / 60) > 0 ? `${Math.floor(audio.duration / 60)}:${String(Math.floor(audio.duration) % 60).padStart(2, '0')}` : "0:00";
 
-    currentTime.textContent =
-        `${Math.floor(audio.currentTime / 60)}:${String(Math.floor(audio.currentTime) % 60).padStart(2, '0')}`;
+  currentTime.textContent =
+    `${Math.floor(audio.currentTime / 60)}:${String(Math.floor(audio.currentTime) % 60).padStart(2, '0')}`;
 
-    full_timelineSlider.max = audio.duration;
-    full_timelineSlider.value = audio.currentTime;
+  full_timelineSlider.max = audio.duration;
+  full_timelineSlider.value = audio.currentTime;
 
-    full_musicDuration.textContent = Math.floor(audio.duration / 60) > 0 ? `${Math.floor(audio.duration / 60)}:${String(Math.floor(audio.duration) % 60).padStart(2, '0')}` : "0:00";;
+  full_musicDuration.textContent = Math.floor(audio.duration / 60) > 0 ? `${Math.floor(audio.duration / 60)}:${String(Math.floor(audio.duration) % 60).padStart(2, '0')}` : "0:00";;
 
-    full_currentTime.textContent = `${Math.floor(audio.currentTime / 60)}:${String(Math.floor(audio.currentTime) % 60).padStart(2, '0')}`;
+  full_currentTime.textContent = `${Math.floor(audio.currentTime / 60)}:${String(Math.floor(audio.currentTime) % 60).padStart(2, '0')}`;
 
-    if (audio.currentTime == audio.duration) {
-        playThisSong()
-    }
+  if (audio.currentTime == audio.duration) {
+    playThisSong()
+  }
 }
 
 function changeMusicCurrentTime() {
-    audio.currentTime = timelineSlider.value;
-    audio.currentTime = full_timelineSlider.value
+  audio.currentTime = timelineSlider.value;
+  audio.currentTime = full_timelineSlider.value
 }
 
 timelineSlider.addEventListener("input", function () {
-    audio.currentTime = timelineSlider.value;
+  audio.currentTime = timelineSlider.value;
 });
 
 const albumCover = document.getElementById("album-cover");
@@ -234,9 +234,9 @@ const albumLength = document.getElementById("album-length");
 const albumSongs = document.getElementById("album-songs");
 
 function changeAlbumContent(artist, albumName) {
-    let songIndex = 1;
+  let songIndex = 1;
 
-    const createAlbumSong = `
+  const createAlbumSong = `
 <button class="album-song" onclick="playThisSong('${artist}', '${albumName}')">
 <h2 class="song-index">${songIndex}</h2>
 <div class="song-name-artists">
@@ -247,60 +247,77 @@ function changeAlbumContent(artist, albumName) {
 <h2 class="song-duration">${Math.floor(artistsObject[artist][albumName].songs[songIndex].duration / 60)}:${String(Math.floor(artistsObject[artist][albumName].songs[songIndex].duration) % 60).padStart(2, '0')}</h2>
 </button>`;
 
-    albumCover.src = `./music-data/album-covers/${String(artist)} - ${String(albumName)}.jpg`;
+  albumCover.src = `./music-data/album-covers/${String(artist)} - ${String(albumName)}.jpg`;
 
-    albumTitle.textContent = artistsObject[artist][albumName].name
+  albumTitle.textContent = artistsObject[artist][albumName].name
 
-    albumArtist.textContent = artistsObject[artist][albumName].artists
-    albumYear.textContent = artistsObject[artist][albumName].year
-    albumLength.textContent = artistsObject[artist][albumName].length + " Songs"
+  albumArtist.textContent = artistsObject[artist][albumName].artists
+  albumYear.textContent = artistsObject[artist][albumName].year
+  albumLength.textContent = artistsObject[artist][albumName].length + " Songs"
 
-    albumSongs.innerHTML = createAlbumSong;
+  albumSongs.innerHTML = createAlbumSong;
 
-    artistsObject[artist][albumName].songs
+  artistsObject[artist][albumName].songs
 }
 
 let previousRandomSong
 
 function playThisSong(artist, albumName, songIndex) {
-    if (artist && albumName /*&& songIndex*/) {
-        let oldAudio = Object(audio.attributes);
+  if (artist && albumName /*&& songIndex*/) {
+    let oldAudio = Object(audio.attributes);
 
-        audio.setAttribute('src', `./music-data/music/${artistsObject[artist][albumName].songs[1].artists} - ${artistsObject[artist][albumName].songs[1].name}.m4a`);
+    audio.setAttribute('src', `./music-data/music/${artistsObject[artist][albumName].songs[1].artists} - ${artistsObject[artist][albumName].songs[1].name}.m4a`);
 
-        isPlaying = false;
-        playPauseButtonBehavior();
+    isPlaying = false;
+    playPauseButtonBehavior();
 
-        musicPlayerSong.textContent = artistsObject[artist][albumName].songs[1].name;
-        musicPlayerArtists.textContent = artistsObject[artist][albumName].songs[1].artists;
-        musicPlayerImage.src = `./music-data/album-covers/${artist} - ${albumName}.jpg`;
+    musicPlayerSong.textContent = artistsObject[artist][albumName].songs[1].name;
+    musicPlayerArtists.textContent = artistsObject[artist][albumName].songs[1].artists;
+    musicPlayerImage.src = `./music-data/album-covers/${artist} - ${albumName}.jpg`;
 
-        full_musicPlayerSong.textContent = musicPlayerSong.textContent;
-        full_musicPlayerArtists.textContent = musicPlayerArtists.textContent;
-        full_musicPlayerImage.src = musicPlayerImage.src;
+    full_musicPlayerSong.textContent = musicPlayerSong.textContent;
+    full_musicPlayerArtists.textContent = musicPlayerArtists.textContent;
+    full_musicPlayerImage.src = musicPlayerImage.src;
 
-        oldAudio = audio.attributes;
-    } else {
-        // Hard Coded Ik that this is bad
+    oldAudio = audio.attributes;
+  } else {
+    // Hard Coded Ik that this is bad
 
-        let artistArray = [["Metro Boomin", "METRO BOOMIN PRESENTS SPIDER-MAN ACROSS THE SPIDER-VERSE (SOUNDTRACK FROM AND INSPIRED BY THE MOTION PICTURE)"], ["Metro Boomin", "HEROS & VILLAINS"], ["Connor Price", "Spinnin"], ["Post Malone", "AUSTIN"], ["Various Artists", "Spider-Man Into the Spider-Verse (Soundtrack From & Inspired by the Motion Picture)"], ["The Weeknd", "Starboy"]];
 
-        let chooseRandomSong
-        for (let i = 0; i < 1; i++) {
-            chooseRandomSong = Math.floor(Math.random() * 6);
-            if (chooseRandomSong != previousRandomSong) {
-                i = 2;
-            }
-        }
+    // TO CHANGE MUSIC HERE 31415
 
-        previousRandomSong = chooseRandomSong;
+    let libraryLength = 9;
 
-        playThisSong(artistArray[chooseRandomSong][0], artistArray[chooseRandomSong][1]);
+    let artistArray =
+      [
+        ["Metro Boomin", "METRO BOOMIN PRESENTS SPIDER-MAN ACROSS THE SPIDER-VERSE (SOUNDTRACK FROM AND INSPIRED BY THE MOTION PICTURE)"],
+        ["Metro Boomin", "HEROS & VILLAINS"],
+        ["Connor Price", "Spinnin"],
+        ["Post Malone", "AUSTIN"],
+        ["Various Artists", "Spider-Man Into the Spider-Verse (Soundtrack From & Inspired by the Motion Picture)"],
+        ["The Weeknd", "Starboy"],
+        ["Travis Scott, Bad Bunny, The Weeknd", "K-POP"],
+        ["Post Malone", "Twelve Carat Toothache"],
+        ["Post Malone", "beerbongs & bentleys"]
+
+      ];
+
+    let chooseRandomSong
+    for (let i = 0; i < 1; i++) {
+      chooseRandomSong = Math.floor(Math.random() * libraryLength);
+      if (chooseRandomSong != previousRandomSong) {
+        i = 2;
+      }
     }
+
+    previousRandomSong = chooseRandomSong;
+
+    playThisSong(artistArray[chooseRandomSong][0], artistArray[chooseRandomSong][1]);
+  }
 }
 
 function skipThisSong() {
-    playThisSong();
+  playThisSong();
 }
 
 const musicPlayerSong = document.getElementById("music-player-song");
@@ -318,21 +335,21 @@ const showFullScreenButton = document.getElementById("show-full-screen");
 let isFullScreenPlayerMobileOpen = false;
 
 function showFullScreen() {
-    if (isFullScreenPlayerMobileOpen) {
-        // FullScreenPlayerMobile.classList.remove("show-full-screen-player-mobile");
-        updateFullscreenY(0.5);
-        isFullScreenPlayerMobileOpen = false;
-        showFullScreenButton.textContent = "expand_less";
-    } else {
-        // FullScreenPlayerMobile.classList.add("show-full-screen-player-mobile");
-        updateFullscreenY(90);
-        isFullScreenPlayerMobileOpen = true;
-        showFullScreenButton.textContent = "expand_more";
-    }
+  if (isFullScreenPlayerMobileOpen) {
+    // FullScreenPlayerMobile.classList.remove("show-full-screen-player-mobile");
+    updateFullscreenY(0.5);
+    isFullScreenPlayerMobileOpen = false;
+    showFullScreenButton.textContent = "expand_less";
+  } else {
+    // FullScreenPlayerMobile.classList.add("show-full-screen-player-mobile");
+    updateFullscreenY(90);
+    isFullScreenPlayerMobileOpen = true;
+    showFullScreenButton.textContent = "expand_more";
+  }
 }
 
 const updateFullscreenY = (translateY) => {
-    FullScreenPlayerMobile.style.transform = `translate(0,${-translateY}%)`
+  FullScreenPlayerMobile.style.transform = `translate(0,${-translateY}%)`
 }
 
 /*
